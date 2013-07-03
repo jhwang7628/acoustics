0d232bb1e6cd79bd55921d6e6e5bd51245ed46a6
Jeff Chadwick <chadwick@poisson.(none)>
2013-07-03 11:26:12 -0400

    Missing commits.

---
+++ src/visual/Renderer.h
@@ -0,0 +1,11 @@
+#ifndef VISUAL_RENDERER_H
+#   define VISUAL_RENDERER_H
+
+class Renderer
+{
+    public:
+        virtual void render()  { }
+        virtual void render_with_names() { }
+};
+
+#endif
