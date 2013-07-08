55bcefb3cca05323f26a5b232fba0e6abdfb4b25
Jeff Chadwick <chadwick@poisson.(none)>
2013-07-08 14:49:00 -0400

    Added the wrong files.

---
+++ src/tools/init-rigid-tools/write_inertia.cpp
@@ -0,0 +1,178 @@
+/*
+ * compute the volumn and three principle values & correspoding eigenvectors 
+ * of the inertia tensor
+ */
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include "geometry/FixVtxTetMesh.hpp"
+#include "io/TetMeshReader.hpp"
+#include "utils/tuple.hpp"
+
+using namespace std;
+
+typedef FixVtxTetMesh<double>       TTetMesh;
+
+static string tetfileptn;
+static int startid, endid;
+
+void writeInertia( const Matrix3<double> &I, const char *filePrefix )
+{
+  char                       fileName[ 1024 ];
+  double                     value;
+
+  sprintf( fileName, "%s_inertia.3matrix", filePrefix );
+
+  FILE                      *file = fopen( fileName, "wb" );
+
+  if ( !file )
+  {
+    cerr << "Error: could not open " << fileName << " for writing" << endl;
+    exit(1);
+  }
+
+  // Write the matrix in row major format
+  value = I.cols[ 0 ].x;
+  fwrite( (void *)&value, sizeof( double ), 1, file );
+  value = I.cols[ 1 ].x;
+  fwrite( (void *)&value, sizeof( double ), 1, file );
+  value = I.cols[ 2 ].x;
+  fwrite( (void *)&value, sizeof( double ), 1, file );
+
+  value = I.cols[ 0 ].y;
+  fwrite( (void *)&value, sizeof( double ), 1, file );
+  value = I.cols[ 1 ].y;
+  fwrite( (void *)&value, sizeof( double ), 1, file );
+  value = I.cols[ 2 ].y;
+  fwrite( (void *)&value, sizeof( double ), 1, file );
+
+  value = I.cols[ 0 ].z;
+  fwrite( (void *)&value, sizeof( double ), 1, file );
+  value = I.cols[ 1 ].z;
+  fwrite( (void *)&value, sizeof( double ), 1, file );
+  value = I.cols[ 2 ].z;
+  fwrite( (void *)&value, sizeof( double ), 1, file );
+
+  fclose( file );
+}
+
+static double inertia_tensor(const FixVtxTetMesh<double>& mesh,
+                             Matrix3<double>& I, Point3d& x0)
+{
+    const std::vector<double>&    ms  = mesh.masses();
+    const std::vector< Point3d >& vtx = mesh.rest_positions();
+    double mass = 0;
+    x0.zero();
+
+    for(size_t i = 0;i < ms.size();++ i)
+    {
+        mass += ms[i];
+        x0.scaleAdd(ms[i], vtx[i]);
+    }
+    x0 /= mass; // mass center
+
+    I.zero();
+    for(size_t i = 0;i < ms.size();++ i)
+    {
+        Vector3d ri = vtx[i] - x0;
+        I += Matrix3<double>(
+          ms[i]*(M_SQR(ri.y)+M_SQR(ri.z)), -ms[i]*ri.x*ri.y, -ms[i]*ri.x*ri.z,
+          -ms[i]*ri.y*ri.x, ms[i]*(M_SQR(ri.x)+M_SQR(ri.z)), -ms[i]*ri.y*ri.z,
+          -ms[i]*ri.z*ri.x, -ms[i]*ri.z*ri.y, ms[i]*(M_SQR(ri.x)+M_SQR(ri.y)));
+    }
+    return mass;
+}
+
+/* 
+ * Let the inertia matrix of the original object is T
+ * then 
+ *   => V'*T*V = D (D is diagonal matrix)
+ *   => P*V'*T*V*P' = D_ellip (inertia matrix for the corresponding ellipsoid)
+ *   P is the permutation matrix due to the eigenvalue permutation
+ *   e.g. [1 2 3] => [2 3 1]
+ *    P = [ 0 1 0 ;
+ *          0 0 1 ;
+ *          1 0 0 ]
+ */
+int main(int argc, char* argv[])
+{
+#if 0
+    if ( argc != 5 )
+    {
+        cerr << "Invalid arguments!" << endl;
+        cerr << "Usage: " << argv[0] << " <tet mesh filename pattern> <start id> <end id> <output file> " << endl;
+        exit(1);
+    }
+#endif
+
+    if ( argc != 3 )
+    {
+      cerr << "Invalid arguments" << endl;
+      cerr << "Usage: " << argv[ 0 ] << " <tet mesh file name> "
+           << "<output file name>" << endl;
+      exit(1);
+    }
+
+    tetfileptn = argv[1];
+    string outputFilePrefix = argv[2];
+#if 0
+    startid = atoi(argv[2]);
+    endid   = atoi(argv[3]);
+#endif
+
+#if 0
+    ofstream fout(argv[4]);
+    if ( fout.fail() )
+    {
+        cerr << "Cannot open file: " << argv[4] << " for writing" << endl;
+        exit(2);
+    }
+    fout << setprecision(12);
+#endif
+
+#if 0
+    int n = endid - startid + 1;
+    fout << n << endl;
+    char filename[128];
+#endif
+    Matrix3<double> inertia;
+    Point3d mc;
+#if 0
+    for(int i = startid;i <= endid;++ i)
+    {
+        fout << i << ' ';
+
+        sprintf(filename, tetfileptn.c_str(), i);
+        cout << "Reading tet mesh[#" << i << "]: " << filename << endl;
+        TTetMesh tmesh;
+        FV_TetMeshLoader_Double::load_mesh(filename, tmesh);
+        tmesh.init();
+        inertia_tensor(tmesh, inertia, mc);
+        fout << mc.x << ' ' << mc.y << ' ' << mc.z << ' ';
+        fout << inertia.cols[0].x << ' '
+             << inertia.cols[1].x << ' '
+             << inertia.cols[2].x << ' '
+             << inertia.cols[0].y << ' '
+             << inertia.cols[1].y << ' '
+             << inertia.cols[2].y << ' '
+             << inertia.cols[0].z << ' '
+             << inertia.cols[1].z << ' '
+             << inertia.cols[2].z << endl;
+    }
+    fout.close();
+#endif
+    TTetMesh tmesh;
+    FV_TetMeshLoader_Double::load_mesh( tetfileptn.c_str(), tmesh );
+    tmesh.init();
+    inertia_tensor( tmesh, inertia, mc );
+
+#if 0
+    // FIXME
+    inertia *= 6983.0;
+#endif
+
+    writeInertia( inertia, outputFilePrefix.c_str() );
+
+    return 0;
+}
+
---
+++ src/tools/init-rigid-tools/write_mass_center.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include "geometry/FixVtxTetMesh.hpp"
+#include "io/TetMeshReader.hpp"
+
+using namespace std;
+
+typedef FixVtxTetMesh<double>       TTetMesh;
+
+static string tetfileptn;
+static int startid, endid;
+
+void writeMassCenter( Point3d x0, const char *filePrefix )
+{
+  char                       fileName[ 1024 ];
+  double                     value;
+
+  sprintf( fileName, "%s_centerOfMass.3vector", filePrefix );
+
+  FILE                      *file = fopen( fileName, "wb" );
+  
+  if ( !file )
+  {
+    cerr << "Error: could not open " << fileName << " for writing" << endl;
+    exit(1);
+  }
+
+  value = x0.x;
+  fwrite( (void *)&value, sizeof( double ), 1, file );
+  value = x0.y;
+  fwrite( (void *)&value, sizeof( double ), 1, file );
+  value = x0.z;
+  fwrite( (void *)&value, sizeof( double ), 1, file );
+
+  fclose( file );
+}
+
+void writeMass( double mass, const char *filePrefix )
+{
+  char                       fileName[ 1024 ];
+  int                        size = 1;
+
+  sprintf( fileName, "%s_mass.vector", filePrefix );
+
+  FILE                      *file = fopen( fileName, "wb" );
+
+  if ( !file )
+  {
+    cerr << "Error: could not open " << fileName << " for writing" << endl;
+    exit(1);
+  }
+
+  // Write in a format that can be interpreted as a vector of
+  // doubles
+  fwrite( (void *)&size, sizeof( int ), 1, file );
+  fwrite( (void *)&mass, sizeof( double ), 1, file );
+
+  fclose( file );
+}
+
+int main(int argc, char* argv[])
+{
+#if 0
+    if ( argc != 5 )
+    {
+        cerr << "Invalid arguments!" << endl;
+        cerr << "Usage: " << argv[0] << " <tet mesh filename pattern> "
+             << "<start id> <end id> <output file>" << endl;
+        exit(1);
+    }
+#endif
+    if ( argc != 3 )
+    {
+        cerr << "Invalid arguments!" << endl;
+        cerr << "Usage: " << argv[0] << " <tet mesh filename > "
+             << "<output file>" << endl;
+        exit(1);
+    }
+
+    tetfileptn = argv[1];
+    string outputFilePrefix = argv[2];
+#if 0
+    startid = atoi(argv[2]);
+    endid   = atoi(argv[3]);
+#endif
+
+#if 0
+    ofstream fout(argv[4]);
+    if ( fout.fail() )
+    {
+        cerr << "Cannot open file: " << argv[4] << " for writing" << endl;
+        exit(2);
+    }
+
+    char filename[128];
+#endif
+    Point3d x0;
+    double mass;
+#if 0
+    fout << setprecision(22);
+    for(int i = startid; i <= endid; ++ i)
+    {
+        sprintf(filename, tetfileptn.c_str(), i);
+        cout << "Reading tet mesh[#" << i << "]: " << filename << endl;
+        TTetMesh tmesh;
+        FV_TetMeshLoader_Double::load_mesh(filename, tmesh);
+        tmesh.init();
+
+        cerr << i << "  " << tmesh.num_fixed_vertices() << endl;
+        const std::vector<double>&    ms  = tmesh.masses();
+        const std::vector< Point3d >& vtx = tmesh.vertices();
+        cout << "Computing mass center [#" << i << ']' << endl;
+        x0.zero();  mass = 0;
+        for(size_t j = 0;j < ms.size();++ j)
+        {
+            mass += ms[j];
+            x0.scaleAdd(ms[j], vtx[j]);
+        }
+        x0 /= mass;
+        fout << i << ' ' << x0.x << ' ' << x0.y << ' ' << x0.z << endl;
+    }
+#endif
+    TTetMesh tmesh;
+    FV_TetMeshLoader_Double::load_mesh( tetfileptn.c_str(), tmesh );
+    tmesh.init();
+
+    x0.zero();
+    mass = 0.0;
+    const std::vector<double>&    ms  = tmesh.masses();
+    const std::vector< Point3d >& vtx = tmesh.vertices();
+
+    for ( size_t j = 0; j < ms.size(); j++ )
+    {
+      mass += ms[ j ];
+      x0.scaleAdd( ms[ j ], vtx[ j ] );
+    }
+    x0 /= mass;
+
+#if 0
+    // FIXME:
+    mass *= 6983.0;
+#endif
+
+    writeMassCenter( x0, outputFilePrefix.c_str() );
+    writeMass( mass, outputFilePrefix.c_str() );
+
+    return 0;
+}
