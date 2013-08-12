a95c9348dab7324ae23e4953605321fc4d5a8a23
Jeff Chadwick <jchadwick@jchadwick-ubuntu.(none)>
2013-08-12 15:20:42 -0400

    Minor changes.

--- src/tools/rigidsim-gui/RigidSim.h
+++ src/tools/rigidsim-gui/RigidSim.h
@@ -32,9 +32,12 @@ class RigidSim : public QMainWindow, private Ui_RigidSim
             QObject::connect(actionStart, SIGNAL(triggered()), this, SLOT(start()));
             QObject::connect(actionStep,  SIGNAL(triggered()), this, SLOT(step()));
 
-            QObject::connect(actionDropObjects,  SIGNAL(triggered()), this, SLOT(demo_drop_objects()));
-            QObject::connect(actionDropObjsWithFixed, SIGNAL(triggered()), this, SLOT(demo_drop_objs_with_fixed()));
-            QObject::connect(actionShakingPiggy, SIGNAL(triggered()), this, SLOT(demo_shaking_piggy()));
+            QObject::connect(actionDropObjects,  SIGNAL(triggered()), this,
+                             SLOT(demo_drop_objects()));
+            QObject::connect(actionDropObjsWithFixed, SIGNAL(triggered()), this,
+                             SLOT(demo_drop_objs_with_fixed()));
+            QObject::connect(actionShakingPiggy, SIGNAL(triggered()), this,
+                             SLOT(demo_shaking_piggy()));
         }
 
         ~RigidSim();
