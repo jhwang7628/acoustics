2092a7a7badeb26bcf485b400014fb68b140fc02
jui-hsien wang <jw969@cornell.edu>
2015-09-29 17:00:30 -0700

    fixed findlibconfig

--- cmake/FindLibConfig.cmake
+++ cmake/FindLibConfig.cmake
@@ -1,16 +1,17 @@
 # - Find library libConfig++ 
 IF ( LibConfig_DIR )
-    FIND_PATH(LibConfig_INCLUDE_DIR libconfig.h++
+    FIND_PATH(LibConfig_INCLUDE_DIR libconfig.h
         PATHS ${LibConfig_DIR}/include)
-    FIND_LIBRARY(LibConfig_LIBRARY config++
+    FIND_LIBRARY(LibConfig_LIBRARY config
         PATHS ${LibConfig_DIR}/lib)
 ELSE ( LibConfig_DIR )
-    FIND_PATH(LibConfig_INCLUDE_DIR libconfig.h++
+    FIND_PATH(LibConfig_INCLUDE_DIR libconfig.h
         PATHS ${SYSTEM_INC_PATH}
-        PATHS $ENV{INCLUDE})
-    FIND_LIBRARY(LibConfig_LIBRARY config++
+        PATHS $ENV{INCLUDE}
+        PATHS $ENV{CPLUS_INCLUDE_PATH})
+    FIND_LIBRARY(LibConfig_LIBRARY config
         PATHS ${SYSTEM_LIB_PATH}
-        PATHS $ENV{INCLUDE})
+        PATHS $ENV{LD_LIBRARY_PATH})
 ENDIF ( LibConfig_DIR )
 
 IF (LibConfig_INCLUDE_DIR AND LibConfig_LIBRARY)
@@ -19,6 +20,7 @@ IF (LibConfig_INCLUDE_DIR AND LibConfig_LIBRARY)
     ENDIF (NOT LibConfig_FIND_QUIETLY)
 ELSE (LibConfig_INCLUDE_DIR AND LibConfig_LIBRARY)
     IF (LibConfig_FIND_REQUIRED)
+        message( ${LibConfig_INCLUDE_DIR} " : " ${LibConfig_LIBRARY} )
         MESSAGE(FATAL_ERROR "Could not find libConfig")
     ENDIF(LibConfig_FIND_REQUIRED)
 ENDIF (LibConfig_INCLUDE_DIR AND LibConfig_LIBRARY)
