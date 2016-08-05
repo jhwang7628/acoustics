eaa0b561e88425dee8ad5ffdd25d3fbddf5d044e
jui-hsien wang <jw969@cornell.edu>
2016-08-05 15:17:02 -0700

    updated FindEigen.cmake from the commit near-field:4ff3db7

--- cmake/FindEigen.cmake
+++ cmake/FindEigen.cmake
@@ -1,12 +1,7 @@
-find_path(Eigen_INCLUDE_DIR Eigen/Core                                                                                      
-  HINTS "${EIGEN_ROOT}"                                                                                                
-  HINTS "/usr/local/Cellar/"
-  # HINTS "/home/jw969/opt/eigen"
-  PATH_SUFFIXES "eigen"                                                                                                
-  DOC "The directory where Eigen/Core resides"                                                                                                
+find_path(Eigen_INCLUDE_DIR Eigen/Core
+  HINTS $ENV{EIGEN_ROOT}
+  DOC "The directory where Eigen/Core resides"
 )                                                                                                                                             
-                                                                                                                                              
-mark_as_advanced(Eigen_INCLUDE_DIR)                                                                                                           
-                                                                                                                                              
-include(FindPackageHandleStandardArgs)                                                                                                        
+mark_as_advanced(Eigen_INCLUDE_DIR)
+include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(Eigen DEFAULT_MSG Eigen_INCLUDE_DIR) 
