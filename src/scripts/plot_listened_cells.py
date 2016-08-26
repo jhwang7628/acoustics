535366e3adbce0609fe79b42d5118eea36bd7adb
jui-hsien wang <jw969@cornell.edu>
2016-08-25 23:31:08 -0700

    scripts update: separated the plotting from wav output so I can use remotely

--- src/scripts/plot_listened_cells.py
+++ src/scripts/plot_listened_cells.py
@@ -32,30 +32,35 @@ for f in filenames:
 maxValue = np.absolute(listenedData).max()
 print 'Normalize all data by max value = %f' %(maxValue)
 
-# plotting
-colors = [cm.jet(x) for x in np.linspace(1, 0, N_points)]
-plt.figure()
+# writing the wav files
 for ii in range(N_points): 
-    plt.plot(listenedData[:,ii], label=listeningPositions[ii, :], color=colors[ii], linewidth=2) 
     scipy.io.wavfile.write('point_%u.wav' %(ii), stepRate, listenedData[:,ii]/maxValue)
-plt.legend(loc=2)
-plt.xlabel('frame') 
-plt.ylabel('Pressure (Pascal)')
-# plt.title('Monitor points for ball drop using ghost cell')
 
-if len(sys.argv) == 5: 
-    for plot_index in range(N_points):
-        # plot_index = int(sys.argv[4]) 
-        data = listenedData[:, plot_index] 
-        plt.figure() 
-        plt.plot(data[::4], label=listeningPositions[plot_index, :]) 
-        plt.xlabel('frame') 
-        plt.ylabel('Pressure (Pascal)')
-        print 'Listening point = ', listeningPositions[plot_index, :]
-        # try to extract the max in the last couple of cycles
-        extractEndPercentage = 0.2
-        N_extract = N_steps * extractEndPercentage
-        dataExtracted = data[-N_extract::]
-        print 'End %.2f percent of the listened data was extracted. Its max = %f' %(extractEndPercentage*100.0, np.absolute(dataExtracted).max())
-
-# plt.show()
+plotting = False
+if plotting:
+    # plotting
+    colors = [cm.jet(x) for x in np.linspace(1, 0, N_points)]
+    plt.figure()
+    for ii in range(N_points): 
+        plt.plot(listenedData[:,ii], label=listeningPositions[ii, :], color=colors[ii], linewidth=2) 
+    plt.legend(loc=2)
+    plt.xlabel('frame') 
+    plt.ylabel('Pressure (Pascal)')
+    # plt.title('Monitor points for ball drop using ghost cell')
+    
+    if len(sys.argv) == 5: 
+        for plot_index in range(N_points):
+            # plot_index = int(sys.argv[4]) 
+            data = listenedData[:, plot_index] 
+            plt.figure() 
+            plt.plot(data[::4], label=listeningPositions[plot_index, :]) 
+            plt.xlabel('frame') 
+            plt.ylabel('Pressure (Pascal)')
+            print 'Listening point = ', listeningPositions[plot_index, :]
+            # try to extract the max in the last couple of cycles
+            extractEndPercentage = 0.2
+            N_extract = N_steps * extractEndPercentage
+            dataExtracted = data[-N_extract::]
+            print 'End %.2f percent of the listened data was extracted. Its max = %f' %(extractEndPercentage*100.0, np.absolute(dataExtracted).max())
+    
+    # plt.show()
