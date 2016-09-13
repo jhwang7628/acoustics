93cedc2a02cd91c896be630c55675a546e0afba3
jui-hsien wang <jw969@cornell.edu>
2016-09-13 10:02:24 -0700

    added average duration capability to SimpleTimer class

--- src/utils/SimpleTimer.h
+++ src/utils/SimpleTimer.h
@@ -11,13 +11,15 @@ class SimpleTimer
     private:
         std::chrono::high_resolution_clock::time_point  _start;
         std::chrono::high_resolution_clock::duration    _elapsed;
+        int _count = 0; 
 
     public:
         SimpleTimer() : _elapsed(0) {}
 
-        inline void Start() { _start = std::chrono::high_resolution_clock::now(); }
-        inline void Pause() { _elapsed += std::chrono::high_resolution_clock::now() - _start;}
+        inline void Start() { _start = std::chrono::high_resolution_clock::now();}
+        inline void Pause() { _elapsed += std::chrono::high_resolution_clock::now() - _start; _count++; }
         inline double Duration() { return (std::chrono::duration_cast<std::chrono::duration<double>>(_elapsed)).count(); }
+        inline double DurationAverage() { return _count>0 ? Duration()/(double)_count : -1.0; }
 
         //// debug methods ////
         inline void DurationDebug()
