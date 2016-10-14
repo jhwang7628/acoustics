a070ab6cd4ee7b8fac5ece3c6a0092881b6b026c
jui-hsien wang <jw969@cornell.edu>
2016-10-14 12:51:55 -0700

    minor misplaced timer pause call

--- src/wavesolver/PML_WaveSolver.cpp
+++ src/wavesolver/PML_WaveSolver.cpp
@@ -600,8 +600,8 @@ void PML_WaveSolver::stepCollocated()
     _cellClassifyTimer.pause(); 
     _freshCellTimer.start(); 
     _grid.InterpolateFreshPressureCell(pLast, _timeStep, _currentTime, _density);  
-    _freshCellTimer.pause(); 
     _grid.InterpolateFreshPressureCell(pCurr, _timeStep, _currentTime, _density);  
+    _freshCellTimer.pause(); 
     _ghostCellTimer.start(); 
     _grid.PML_pressureUpdateGhostCells_Jacobi(pCurr, _pGhostCellsFull, _timeStep, _waveSpeed, _currentTime, _density); 
     _ghostCellTimer.pause(); 
