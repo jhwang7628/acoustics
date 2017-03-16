e1f9399223b098a16cdd7ed376db40da530fdcb3
jui-hsien wang <jw969@cornell.edu>
2017-03-16 15:40:36 -0700

    leave only harmonic vibrational source

--- src/wavesolver/FDTD_AcousticSimulator.cpp
+++ src/wavesolver/FDTD_AcousticSimulator.cpp
@@ -25,20 +25,19 @@ _SetBoundaryConditions()
     {
         RigidSoundObjectPtr objectPtr = _sceneObjects->GetPtr(index);
         // add modal vibrational source
-        VibrationalSourcePtr sourcePtr(new ModalVibrationalSource(objectPtr)); 
-        objectPtr->AddVibrationalSource(sourcePtr); 
+        //VibrationalSourcePtr sourcePtr(new ModalVibrationalSource(objectPtr)); 
+        //objectPtr->AddVibrationalSource(sourcePtr); 
 
         // add acceleration noise source
-        VibrationalSourcePtr anSourcePtr(new AccelerationNoiseVibrationalSource(objectPtr)); 
-        objectPtr->AddVibrationalSource(anSourcePtr);
+        //VibrationalSourcePtr anSourcePtr(new AccelerationNoiseVibrationalSource(objectPtr)); 
+        //objectPtr->AddVibrationalSource(anSourcePtr);
 
         // add debug harmonic source
-        //const REAL omega = 2.0*M_PI*500.0;
-        //const REAL phase = 0.0;
+        const REAL omega = 2.0*M_PI*500.0;
+        const REAL phase = 0.0;
+        VibrationalSourcePtr sourcePtr(new HarmonicVibrationalSource(objectPtr, omega, phase)); 
         //VibrationalSourcePtr sourcePtr(new HarmonicVibrationalSource(objectPtr, omega, phase, 1000., 0.0)); 
-        //VibrationalSourcePtr sourcePtr(new HarmonicVibrationalSource(objectPtr, omega, phase, 1000., 0.0)); 
-        //objectPtr->AddVibrationalSource(sourcePtr); 
-        //objectPtr->AddVibrationalSource(sourcePtr); 
+        objectPtr->AddVibrationalSource(sourcePtr); 
 
         //objectPtr->TestObjectBoundaryCondition();
     }
