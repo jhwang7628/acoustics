f7dbf354de9a0fae8870c386e80b92c0263f0eca
Tim Langlois <tlangloi@adobe.com>
2017-04-12 18:25:54 +0000

    starting bubble vibrational source code

---
+++ src/wavesolver/WaterVibrationalSourceBubbles.cpp
@@ -0,0 +1,72 @@
+#include <wavesolver/WaterVibrationalSourceBubbles.h>
+#include <wavesolver/FDTD_RigidSoundObject.h>
+#include <wavesolver/Wavesolver_ConstantsAndTypes.h>
+#include <sndgen/WavReader.hpp>
+#include <utils/STL_Wrapper.h>
+#include <fstream>
+
+//##############################################################################
+//##############################################################################
+WaterVibrationalSourceBubbles::
+WaterVibrationalSourceBubbles(RigidObjectPtr owner, const std::string &wavFile, const REAL &decayRadius)
+    : VibrationalSource(owner), _surfaceMesh(owner->GetMeshPtr()), _decayRadius(decayRadius)
+{
+    Initialize(wavFile);
+}
+
+//##############################################################################
+//##############################################################################
+REAL WaterVibrationalSourceBubbles::
+Evaluate(const Vector3d &position, const Vector3d &normal, const REAL &time, const int &hintTriangle)
+{
+    if (normal.dotProduct(_wantedNormal)/normal.length() > _validAngleThreshold)
+    {
+        // TODO: return acceleration at this point
+    }
+    else
+        return 0.0;
+}
+
+//##############################################################################
+//##############################################################################
+REAL WaterVibrationalSourceBubbles::
+Evaluate(const int &vertexID, const Vector3d &vertexNormal, const REAL &time)
+{
+    throw std::runtime_error("**ERROR** Cannot sample water vibrational source using vertexID");
+    return 0.0;
+}
+
+//##############################################################################
+//##############################################################################
+REAL WaterVibrationalSourceBubbles::
+EvaluateVelocity(const Vector3d &position, const Vector3d &normal, const REAL &time)
+{
+    if (normal.dotProduct(_wantedNormal)/normal.length() > _validAngleThreshold)
+    {
+        const REAL decay = Decay(position);
+        return _interpolatorVelocity.eval(time) * decay;
+    }
+    else
+        return 0.0;
+}
+
+//##############################################################################
+//##############################################################################
+REAL WaterVibrationalSourceBubbles::
+EvaluateDisplacement(const Vector3d &position, const Vector3d &normal, const REAL &time)
+{
+    throw std::runtime_error("**ERROR** not implemented");
+}
+
+//##############################################################################
+//##############################################################################
+void WaterVibrationalSourceBubbles::
+Initialize(const std::string &wavFile)
+{
+    std::cout << "Initialize WaterVibrationalSource with file: " << wavFile << std::endl;
+    ReadOscillatorFromWav(wavFile);
+    ComputeVelocityAndAcceleration();
+    PrecomputeInterpolation();
+    InitializeDecayModel();
+}
+
---
+++ src/wavesolver/WaterVibrationalSourceBubbles.h
@@ -0,0 +1,35 @@
+#ifndef WATER_VIBRATIONAL_SOURCE_BUBBLES_H
+#define WATER_VIBRATIONAL_SOURCE_BUBBLES_H
+
+#include <TYPES.h>
+#include <wavesolver/VibrationalSource.h>
+#include <wavesolver/FDTD_RigidObject.h>
+#include <wavesolver/Wavesolver_ConstantsAndTypes.h>
+
+//##############################################################################
+// This class handles source evaluation for water surface objects using data from the bubbles project.
+//##############################################################################
+class WaterVibrationalSource : public VibrationalSource
+{
+    public:
+        typedef std::shared_ptr<TriangleMesh<REAL> > TriangleMeshPtr;
+
+    private:
+        Vector3d            _wantedNormal = Vector3d(0, 1, 0);
+        REAL                _validAngleThreshold = 0.5; // use to determine if the vertex has source. See Evaluate() for usage. default: 0.5
+        TriangleMeshPtr     _surfaceMesh;
+        REAL                _sampleRate;
+        REAL                _startTime = 0.0;
+
+    public:
+        WaterVibrationalSourceBubbles(RigidObjectPtr owner, const std::string &wavFile, const REAL &decayRadius);
+
+        virtual REAL Evaluate(const Vector3d &position, const Vector3d &normal, const REAL &time, const int &hintTriangle);
+        virtual REAL Evaluate(const int &vertexID, const Vector3d &vertexNormal, const REAL &time);
+        virtual REAL EvaluateVelocity(const Vector3d &position, const Vector3d &normal, const REAL &time);
+        virtual REAL EvaluateDisplacement(const Vector3d &position, const Vector3d &normal, const REAL &time);
+
+        void Initialize(const std::string &dataDir);
+};
+
+#endif
