3410f892771a9f6be3cb80871531bf16906407a2
jui-hsien wang <jw969@cornell.edu>
2017-10-02 18:06:04 -0700

    shells can now be rasterized onto wavesolver. tested it with static shell with hand-crafted dipole source

--- src/wavesolver/FDTD_ShellObject.cpp
+++ src/wavesolver/FDTD_ShellObject.cpp
@@ -1,1 +1,36 @@
 #include "wavesolver/FDTD_ShellObject.h"
+//##############################################################################
+// Function _BuildDistanceField
+//##############################################################################
+void FDTD_ShellObject::
+Initialize()
+{
+    // find bounding box 
+    Point3d  b_l, b_h, b_c; 
+    _mesh->bounding_box(b_l, b_h); 
+    b_c = (b_l + b_h)/2.0;
+    const Vector3d h = (b_h - b_l)/2.0*1.05; // enlarge by 105%
+    b_l = b_c - h; 
+    b_h = b_c + h; 
+    _bbox = BoundingBox(b_l, b_h); 
+    _init = true; 
+}
+  
+//##############################################################################
+// Function DistanceToMesh
+//##############################################################################
+REAL FDTD_ShellObject:: 
+DistanceToMesh(const Vector3d &position)
+{
+    if (!_init)
+        Initialize();
+    if (!_bbox.Inside(position))
+        return std::numeric_limits<REAL>::max(); 
+    const auto p_o = WorldToObjectPoint(position);
+    Vector3d bp, pp; //boundary, projected
+    int tri_idx; 
+    REAL d; 
+#pragma omp critical
+    d = _mesh->ComputeClosestPointOnMesh(p_o, pp, tri_idx, pp);
+    return d;
+}
--- src/wavesolver/FDTD_ShellObject.h
+++ src/wavesolver/FDTD_ShellObject.h
@@ -1,12 +1,16 @@
 #ifndef FDTD_SHELLOBJECT_H
 #define FDTD_SHELLOBJECT_H
 #include "config.h"
+#include "geometry/BoundingBox.h"
 #include "wavesolver/FDTD_RigidObject.h"
 //##############################################################################
 // Class FDTD_ShellObject
 //##############################################################################
 class FDTD_ShellObject : public FDTD_RigidObject
 {
+private:
+    bool _init = false; 
+    BoundingBox _bbox; 
 public:
     FDTD_ShellObject()
         : FDTD_RigidObject(SHELL_OBJ)
@@ -26,13 +30,14 @@ public:
     {}
 
     virtual REAL DistanceToMesh(const double &x, const double &y, const double &z)
-    {return std::numeric_limits<REAL>::max();}
-    virtual REAL DistanceToMesh(const Vector3d &position)
-    {return DistanceToMesh(position.x, position.y, position.z);}
+    {const Vector3d pos(x,y,z); return DistanceToMesh(pos);}
+    virtual REAL DistanceToMesh(const Vector3d &position); 
     virtual bool NormalToMesh(const double &x, const double &y, const double &z, 
                               Vector3d &queriedNormal)
     {return false;}
     virtual bool NormalToMesh(const Vector3d &position, Vector3d &queriedNormal)
     {return NormalToMesh(position.x, position.y, position.z, queriedNormal);}
+
+    void Initialize(); 
 };
 #endif
