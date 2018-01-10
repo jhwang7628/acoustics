c28fa38dbe136b3e856274538ad811a5859d649b
jui-hsien wang <jw969@cornell.edu>
2018-01-10 11:24:28 -0800

    scripts: added simpler audio output for multiple speakers along ray, WIP: integrate it to main postprocessing pipeline

--- src/scripts/read_single_wavesolver_audio.py
+++ src/scripts/read_single_wavesolver_audio.py
@@ -12,8 +12,7 @@ if len(sys.argv) < 3:
 
 results = Wavesolver_Results()
 results.Set_Folder(sys.argv[1])
-data = results.Read_All_Audio(False, 1, None, None, filename=sys.argv[2])
-print data.shape
+data = results.Read_All_Audio(False, 1, None, None, filename=sys.argv[2], N_speakers_along_ray=30)
 
 if len(sys.argv) > 3:
     dt = 1./float(sys.argv[3])
@@ -28,3 +27,27 @@ else:
 plt.figure()
 plt.plot(t, data)
 plt.show()
+
+sampfreq = 1./dt
+wavfreq  = 44100
+prefix   = 'point'
+wavformat= '16int'
+out_dir  = 'post'
+N_points = data.shape[1]
+rateRatio = float(sampfreq) / float(wavfreq)
+for ii in range(N_points):
+    print 'point %u' %(ii)
+    # scipy resample uses FFT, zero-pad it to make it fast
+    outputdata = data[:,ii].copy()
+    nextpow2 = int(np.power(2, np.floor(np.log2(len(outputdata)))+1))
+    T = float(outputdata.shape[0])/float(sampfreq)
+    outputdata = np.pad(outputdata, (0, nextpow2-len(outputdata)), 'constant', constant_values=(0.,0.))
+    outputdata = signal.resample(outputdata, int(float(len(outputdata))/rateRatio))
+    outputdata = outputdata[:int(round(T*wavfreq))]
+    normalization = np.absolute(outputdata).max()
+    if wavformat == '32float':
+        finaldata = outputdata/normalization
+    elif wavformat == '16int':
+        finaldata = ((outputdata/normalization*32767)).astype('int16')
+    scipy.io.wavfile.write('%s/%s_%u.wav' %(out_dir, prefix, ii), wavfreq, finaldata)
+
