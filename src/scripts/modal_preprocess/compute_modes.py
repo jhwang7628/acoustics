8fcac8870fe82ac318b9238b8dd3e91c5f3d20ce
Jui-Hsien Wang <juiwang@adobe.com>
2022-04-01 11:15:12 -0700

    print python3 style

--- src/scripts/modal_preprocess/compute_modes.py
+++ src/scripts/modal_preprocess/compute_modes.py
@@ -20,19 +20,19 @@ def ReadMaterialFile(filename):
             rho = float(tokens[0])
             youngs = float(tokens[1])
             poisson = float(tokens[2])
-    print 'Parsed material for file: %s' %(tokens[-1])
-    print ' density        : %f' %(rho)
-    print ' youngs modulus : %e' %(youngs)
-    print ' poisson ratio  : %.2f' %(poisson)
+    print('Parsed material for file: %s' %(tokens[-1]))
+    print(' density        : %f' %(rho))
+    print(' youngs modulus : %e' %(youngs))
+    print(' poisson ratio  : %.2f' %(poisson))
     return rho, youngs, poisson
 
 ################################################################################
 ################################################################################
 if __name__ == '__main__':
     if len(sys.argv) != 6:
-        print '**Usage: %s <obj_prefix> <material_file> <num_eigenvalues> <output_folder> <output_file>' %(sys.argv[0])
+        print('**Usage: %s <obj_prefix> <material_file> <num_eigenvalues> <output_folder> <output_file>' %(sys.argv[0]))
         sys.exit()
-    print '**NOTE** Make sure you run this in the directory of obj/tet files'
+    print('**NOTE** Make sure you run this in the directory of obj/tet files')
 
     ## User defined settings
     installPath = projectPath()
@@ -41,9 +41,9 @@ if __name__ == '__main__':
     redirect = '/dev/stdout';
 
     ## Automatic
-    print '################################################################################'
-    print '## Initialization'
-    print '################################################################################'
+    print('################################################################################')
+    print('## Initialization')
+    print('################################################################################')
     objName = sys.argv[1]
     matName = sys.argv[2]
     numEigs = int(sys.argv[3])
@@ -54,32 +54,32 @@ if __name__ == '__main__':
     bin_arpack_eigensolver='%s/arpack-eigensolver' %(binPath)
     density, youngsModulus, poissonRatio = ReadMaterialFile(matName)
     if not os.path.isfile(bin_elasticity) or not os.path.isfile(bin_arpack_eigensolver):
-        print '**ERROR** some binaries required are not found in path %s' %(binPath)
-    print '\n\n'
+        print('**ERROR** some binaries required are not found in path %s' %(binPath))
+    print('\n\n')
 
     ## Pipeline starts
-    print '################################################################################'
-    print '## Generate mass and stiff matrix'
-    print '################################################################################'
+    print('################################################################################')
+    print('## Generate mass and stiff matrix')
+    print('################################################################################')
     cmd = '%s %s %f %f' %(bin_elasticity, tetFile, youngsModulus, poissonRatio);
-    print cmd;
-    print '........................................'
+    print(cmd)
+    print('........................................')
     os.system('%s > %s' %(cmd, redirect));
-    print '\n\n'
+    print('\n\n')
 
     # This step also generates <objName>.tet.geo.txt, but we want to rename this
     os.system('mv -f %s.geo.txt %s.geo.txt' %(tetFile, objName));
 
-    print '################################################################################'
-    print '## Linear modal analysis'
-    print '################################################################################'
+    print('################################################################################')
+    print('## Linear modal analysis')
+    print('################################################################################')
     # Using a threshold of 1.0 here seems to work
     cmd = ("%s -n %d -t 1.0 -s %s_stiffness.mat -m %s_mass.mat -o %s/%s -v") %(bin_arpack_eigensolver, numEigs, tetFile, tetFile, outdir, outfile);
-    print cmd;
-    print '........................................'
+    print(cmd)
+    print('........................................')
     os.system('%s > %s' %(cmd, redirect));
     cmd = 'mv %s.geo.txt %s*_mass.mat %s*_mass.bcsm %s*_stiffness.mat %s*_stiffness.bcsm %s' %(objName, tetFile, tetFile, tetFile, tetFile, outdir)
-    print cmd
+    print(cmd)
     os.system('%s > %s' %(cmd, redirect));
 
-    print '\n\n'
+    print('\n\n')
--- src/scripts/modal_preprocess/write_mode_frequencies.py
+++ src/scripts/modal_preprocess/write_mode_frequencies.py
@@ -6,13 +6,13 @@ from mode_data_structures import Modes
 ################################################################################
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print '**Usage: %s <modes_file> <density> <out_file>' %(sys.argv[0])
+        print('**Usage: %s <modes_file> <density> <out_file>' %(sys.argv[0]))
         sys.exit(1)
 
     modes_file = sys.argv[1]
     density = float(sys.argv[2])
     modes = Modes.Read_Modes(modes_file)
-    print modes.num_modes
+    print(modes.num_modes)
     freqs = modes.Get_All_Frequencies(density)
 
     np.savetxt(sys.argv[3], freqs)
